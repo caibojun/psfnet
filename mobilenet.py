85068072ba9501febe1108b27fed72cd60308c3a
caibojun <582168962@qq.com>
2019-05-24 22:23:19 +0800

    modified instance norm

--- mobilenet.py
+++ mobilenet.py
@@ -50,10 +50,12 @@ def mobilenet(inputs,
                                                   kernel_size=[3, 3],
                                                   scope=sc+'/depthwise_conv')
 
+        bn = slim.batch_norm(depthwise_conv, scope=sc+'/dw_batch_norm')
         pointwise_conv = slim.convolution2d(bn,
                                         num_pwc_filters,
                                         kernel_size=[1, 1],
                                         scope=sc+'/pointwise_conv')
+        bn = slim.batch_norm(pointwise_conv, scope=sc+'/pw_batch_norm')
         return bn
   
     def deconv2d(input_, output_dim, ks=4, s=2, stddev=0.02, sc="deconv2d"):
